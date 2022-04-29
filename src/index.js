a21910148f7d3a24f1f0ade1e4bbc30645163774
lenny1596 <70401444+lenny1596@users.noreply.github.com>
2022-04-29 22:42:36 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const testPage = (
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Biriyani</li>
+      <li>Burger</li>
+      <li>Shwarma</li>
+      <li>Chicken Wings</li>
+    </ul>
+  </div>
+);
+root.render(testPage);
